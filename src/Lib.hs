7578559de141ed4c18f187cf495f6aa91ac9053c
Caio Rulli Thomaz <caio@crthomaz.com.br>
2021-05-25 22:04:41 -0300

    Parser is actually working!

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib
-    ( run
-    ) where
+  ( run,
+  )
+where
 
 import Data.Char
 
@@ -8,39 +9,51 @@ someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
 data LexicalValue = Number Int | Variable String deriving (Eq, Show)
+
 data Token = Open | Close | Element LexicalValue deriving (Eq, Show)
 
 lexer :: String -> LexicalValue
 lexer s
   | all isNumber s = Number . read $ s
-  | otherwise      = Variable s
-
-openOrClose :: Char -> Bool
-openOrClose '(' = True
-openOrClose ')' = True
-openOrClose _   = False
+  | otherwise = Variable s
 
 tokenizeWord :: String -> [Token]
 tokenizeWord [] = []
-tokenizeWord s@(c:cs)
-  | c == '('  = Open : tokenizeWord cs
-  | c == ')'  = Close : tokenizeWord cs
-  | otherwise = let (element, rest) = break openOrClose s
-                in  Element (lexer element) : tokenizeWord rest
+tokenizeWord s@(c : cs)
+  | c == '(' = Open : tokenizeWord cs
+  | c == ')' = Close : tokenizeWord cs
+  | otherwise =
+    let (element, rest) = break (\c -> c == '(' || c == ')') s
+     in Element (lexer element) : tokenizeWord rest
 
 tokenize :: String -> [Token]
 tokenize input =
   let inputWords = words input
-  in concatMap tokenizeWord inputWords
+   in concatMap tokenizeWord inputWords
 
 data Sexpr = Atom LexicalValue | Node [Sexpr] deriving (Show)
 
+type Depth = Int
+
+breakOnMatchingClose :: Depth -> [Token] -> ([Token], [Token])
+breakOnMatchingClose 0 (Close : ts) = ([], ts)
+breakOnMatchingClose depth (Open : ts) =
+  let (nextA, nextB) = breakOnMatchingClose (depth + 1) ts
+   in (Open : nextA, nextB)
+breakOnMatchingClose depth (Close : ts) =
+  let (nextA, nextB) = breakOnMatchingClose (depth - 1) ts
+   in (Close : nextA, nextB)
+breakOnMatchingClose depth (t : ts) =
+  let (nextA, nextB) = breakOnMatchingClose depth ts
+   in (t : nextA, nextB)
+
 parse :: [Token] -> [Sexpr]
 parse [] = []
-parse ((Element lexVal):ts) = Atom lexVal : parse ts
-parse (Open:ts) = let (sexpr, rest) = break (== Close) ts
-                  in Node (parse sexpr) : parse rest
-parse (Close:ts) = parse ts
-  
+parse ((Element lexVal) : ts) = Atom lexVal : parse ts
+parse (Open : ts) =
+  let (sexpr, rest) = breakOnMatchingClose 0 ts
+   in Node (parse sexpr) : parse rest
+parse (Close : ts) = parse ts
+
 run :: String -> String
 run = show . parse . tokenize
