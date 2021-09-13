afe10a72eb04f4740f32da34982bc5471240254b
Caio Rulli Thomaz <caio@crthomaz.com.br>
2021-09-13 01:08:57 -0300

    Refactor test utils

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,41 +7,38 @@ import FrontEnd
 import BackEnd
 import Primitives
 
-foldStates :: Environment -> [StateT Environment (Either String) Type]
-  -> Either String Type
-foldStates _ [] = Right Nil
-foldStates env [s] = evalStateT s env
-foldStates env (s:ss) = case result of
-  Right resultEnv -> foldStates resultEnv ss
-  Left resultErr -> Left resultErr
-  where result = execStateT s env
-
-run :: String -> Either String Type
-run expr = foldStates initialEnv $ fmap eval sexprs
-  where sexprs = parse . tokenize $ expr
+run :: String -> Type
+run expr = fst $ foldl foldFn (Nil, initialEnv) states
+  where
+    foldFn :: (Type, Environment) -> StateT Environment (Either String) Type -> (Type, Environment)
+    foldFn = \(_, env) st -> case runStateT st env of
+      Left  _           -> (Nil, env)
+      Right (t, newEnv) -> (t, newEnv)
+    states = fmap eval sexprs
+    sexprs = parse . tokenize $ expr
 
 main :: IO ()
 main = hspec $ do
   describe "Base functionality" $ do
     it "World makes sense" $
-      run "(+ 2 2)" `shouldBe` Right (Number 4)
+      run "(+ 2 2)" `shouldBe` Number 4
 
     it "Nested arithmetic works" $
-      run "(- (+ 30 20) 8)" `shouldBe` Right (Number 42)
+      run "(- (+ 30 20) 8)" `shouldBe` Number 42
 
     it "Conditional operations work: true" $
-      run "(if (= (+ 2 2) 4) 100 0)" `shouldBe` Right (Number 100)
+      run "(if (= (+ 2 2) 4) 100 0)" `shouldBe` Number 100
 
     it "Conditional operations work: false" $
-      run "(if (= (+ 2 2) 5) 100 0)" `shouldBe` Right (Number 0)
+      run "(if (= (+ 2 2) 5) 100 0)" `shouldBe` Number 0
 
     it "Can use anonymous functions" $
-      run "((fn (x) (+ x 1)) 68)" `shouldBe` Right (Number 69)
+      run "((fn (x) (+ x 1)) 68)" `shouldBe` Number 69
 
     it "Can define variables" $
       run "(def a 3) \
-          \(+ a 5)" `shouldBe` Right (Number 8)
+          \(+ a 5)" `shouldBe` Number 8
 
     it "Can define functions" $
       run "(defn inc (x) (+ x 1)) \
-          \(inc 5)" `shouldBe` Right (Number 6)
+          \(inc 5)" `shouldBe` Number 6
