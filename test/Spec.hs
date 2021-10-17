53dc73ce11d9b37fae3fee4d31046275b3d87788
Caio Rulli Thomaz <caio@crthomaz.com.br>
2021-10-17 01:07:35 -0300

    Adding tests for parse errors

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,14 +5,17 @@ import Control.Monad.State.Lazy (evalStateT)
 import FrontEnd (nparser)
 import Primitives (initialEnv)
 import Test.Hspec (describe, hspec, it, shouldBe)
-import Text.Megaparsec (parse)
+import Text.Megaparsec (parse, ParseErrorBundle (bundleErrors))
+import Data.Void (Void)
+import Data.Bifunctor (first)
+import Data.List.NonEmpty (toList)
 
-run :: String -> Type
+run :: String -> Either (ParseErrorBundle String Void) Type
 run expr = case result of
-  Left _ -> undefined -- parse error
+  Left bundle -> Left bundle -- parse error
   Right (Left _) -> undefined -- eval error
   Right (Right []) -> undefined -- no result types
-  Right (Right ts) -> last ts
+  Right (Right ts) -> Right $ last ts
   where
     result = (`evalStateT` initialEnv) . sequence <$> states
     states = sequence $ mapM (fmap eval) sexprs
@@ -22,28 +25,35 @@ main :: IO ()
 main = hspec $ do
   describe "Base functionality" $ do
     it "World makes sense" $
-      run "(+ 2 2)" `shouldBe` Number 4
+      run "(+ 2 2)" `shouldBe` Right (Number 4)
 
     it "Nested arithmetic works" $
-      run "(- (+ 30 20) 8)" `shouldBe` Number 42
+      run "(- (+ 30 20) 8)" `shouldBe` Right (Number 42)
 
     it "Conditional operations work: true" $
-      run "(if (= (+ 2 2) 4) 100 0)" `shouldBe` Number 100
+      run "(if (= (+ 2 2) 4) 100 0)" `shouldBe` Right (Number 100)
 
     it "Conditional operations work: false" $
-      run "(if (= (+ 2 2) 5) 100 0)" `shouldBe` Number 0
+      run "(if (= (+ 2 2) 5) 100 0)" `shouldBe` Right (Number 0)
 
     it "Can use anonymous functions" $
-      run "((fn (x) (+ x 1)) 68)" `shouldBe` Number 69
+      run "((fn (x) (+ x 1)) 68)" `shouldBe` Right (Number 69)
 
     it "Can define variables" $
       run
         "(def a 3) \
         \(+ a 5)"
-        `shouldBe` Number 8
+        `shouldBe` Right (Number 8)
 
     it "Can define functions" $
       run
         "(defn inc (x) (+ x 1)) \
         \(inc 5)"
-        `shouldBe` Number 6
+        `shouldBe` Right (Number 6)
+
+  describe "Errors" $ do
+    it "Not closing parens should make it fail" $
+      actual `shouldBe` expected
+      where
+        actual = first (toList . bundleErrors) (run "(+ 0 1")
+        expected = Left []
