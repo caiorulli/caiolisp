0a7593250ffc0103ea254ee7ce6382a2878a16be
Caio Rulli Thomaz <caio@crthomaz.com.br>
2021-11-02 17:51:59 -0300

    Add simple haskeline impl

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,41 @@
 module Main where
 
-import BackEnd
-import Control.Monad.State
-import FrontEnd
-import Primitives
+import BackEnd ( eval, Environment )
+import Control.Monad.State ( MonadIO(liftIO), StateT(runStateT) )
+import FrontEnd ( parser )
+import Primitives ( initialEnv )
+import System.Console.Haskeline (InputT, defaultSettings, getInputLine, outputStrLn, runInputT)
 import Text.Megaparsec (errorBundlePretty, parse)
 
-repl :: Environment -> IO ()
-repl env = do
-  putStr "> "
-  input <- getLine
-  if input == "exit"
-    then return ()
-    else do
-      let parseResult = parse parser "repl" input
-      case parseResult of
+type Repl a = InputT IO a
+
+process :: String -> Environment -> IO Environment
+process input env =
+  let parseResult = parse parser "repl" input
+   in case parseResult of
         Left parserErrors -> do
           putStrLn $ errorBundlePretty parserErrors
-          repl env
-        Right sexprs -> do
-          let evalResult = runStateT (mapM eval sexprs) initialEnv
-          case evalResult of
-            Left s -> do
-              putStrLn s
-              repl env
-            Right (ts, newEnv) -> do
-              mapM_ print ts
-              repl newEnv
+          return env
+        Right sexprs ->
+          let evalResult = runStateT (mapM eval sexprs) env
+           in case evalResult of
+                Left s -> do
+                  putStrLn s
+                  return env
+                Right (ts, newEnv) -> do
+                  mapM_ print ts
+                  return newEnv
+
+repl :: Environment -> Repl ()
+repl env = do
+  input <- getInputLine "> "
+  case input of
+    Nothing -> repl env
+    Just "exit" -> outputStrLn "See ya!"
+    Just expr -> liftIO (process expr env) >>= repl
 
 main :: IO ()
 main = do
   putStrLn "Welcome to Caiolisp!"
   putStrLn "Have fun, and remember your most important goal... conquest!"
-  repl initialEnv
+  runInputT defaultSettings $ repl initialEnv
